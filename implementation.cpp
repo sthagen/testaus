1d536ee680bfa3379849dc2008aedafff89cfc45
Stefan Hagen <stefan@hagen.link>
2024-02-10 00:03:19 +0100

    Extended naive make file based version with switch
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- implementation.cpp
+++ implementation.cpp
@@ -18,3 +18,46 @@ auto spread_logic(int a) -> int {
     }
     return b;
 }
+
+// Do some weird switch stuff
+auto switch_logic(int a) -> int {
+    int const b{42};
+    switch (a) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23: {
+            int const c{2 * a};
+            if (c > b) {
+                return c;
+            } else {
+                return b;
+            }
+            int const dead_code{};
+            return dead_code;
+            break;
+        }
+        default: 
+            return b;
+        
+    }
+}
--- implementation.h
+++ implementation.h
@@ -1,3 +1,4 @@
 // Some forward declarations
 auto triple_max(int, int, int) -> int;
 auto spread_logic(int) -> int;
+auto switch_logic(int) -> int;
