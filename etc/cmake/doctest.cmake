ec46f8adc697ea9b430b4bca15d4a733341fde74
Stefan Hagen <stefan@hagen.link>
2024-02-11 10:41:20 +0100

    Added guards and status messages to some modules
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- etc/cmake/doctest.cmake
+++ etc/cmake/doctest.cmake
@@ -1,6 +1,11 @@
-FetchContent_Declare(
-  doctest
-  GIT_REPOSITORY https://github.com/doctest/doctest.git
-  GIT_TAG v2.4.11 # commit ae7a13539fb71f270b87eb2e874fbac80bc8dda2
-  FIND_PACKAGE_ARGS)
-FetchContent_MakeAvailable(doctest)
+if(ENABLE_DOCTESTS)
+  add_definitions(-DENABLE_DOCTEST_IN_LIBRARY)
+  FetchContent_Declare(
+    doctest
+    GIT_REPOSITORY https://github.com/doctest/doctest.git
+    GIT_TAG v2.4.11 # commit ae7a13539fb71f270b87eb2e874fbac80bc8dda2
+    FIND_PACKAGE_ARGS)
+
+  FetchContent_MakeAvailable(doctest)
+  message(STATUS "doctest enabled and loaded - use doctest::doctest in your target_link_library calls")
+endif()
--- etc/cmake/process_coverage.cmake
+++ etc/cmake/process_coverage.cmake
@@ -1,6 +1,8 @@
 add_custom_target(process_coverage
-      DEPENDS run_tests
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-      COMMENT "Running gcovr to process coverage results"
-      COMMAND gcovr --config gcovr.cfg --exclude _deps --exclude ../test --html-details -o coverage/ --exclude-unreachable-branches --decisions .
+  DEPENDS run_tests
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+  COMMENT "Running gcovr to process coverage results"
+  COMMAND gcovr --config gcovr.cfg --exclude _deps --exclude ../test --html-details -o coverage/ --exclude-unreachable-branches --decisions .
 )
+message(STATUS "Added process_coverage target")
+message(STATUS "After running target process_coverage you can open build/coverage/coverage_details.html to inspect")
