8ab5933cda63353f37e3ea682851117983c9827d
Stefan Hagen <stefan@hagen.link>
2024-02-17 22:42:02 +0100

    Made the test framework switch uses more consistent
    - and enforced editor config indent on test file of catch2
    
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- etc/cmake/execute_tests.cmake
+++ etc/cmake/execute_tests.cmake
@@ -1,7 +1,14 @@
 add_custom_target(execute_tests
-  DEPENDS tests tests_catch2
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   COMMENT "Running ctest to excute tests"
   COMMAND ctest --output-on-failure
 )
+
+if(ENABLE_DOCTESTS)
+  add_dependencies(execute_tests tests)
+endif()
+if(ENABLE_CATCH2)
+  add_dependencies(execute_tests tests_catch2)
+endif()
+
 message(STATUS "Added execute_tests target")
--- test/test_implementation_catch2.cpp
+++ test/test_implementation_catch2.cpp
@@ -6,44 +6,44 @@
 #include "../src/implementation.h"
 
 TEST_CASE("testing the branch coverage reporting for paths within one line via Catch2") {
-    std::vector<std::tuple<int, int, int, int>> tuplets = {
-        // a, b, c, expected
-        {1, 0, 0, 1},
-        {0, 1, 0, 1},
-        {1, 0, 1, 1},
-        {0, 0, 0, 0},
-    };
-    for (const auto tuplet: tuplets) {
-        const auto [a, b, c, expected] = tuplet;
-        CHECK(triple_max(a, b, c) == expected);
-    }
+  std::vector<std::tuple<int, int, int, int>> tuplets = {
+    // a, b, c, expected
+    {1, 0, 0, 1},
+    {0, 1, 0, 1},
+    {1, 0, 1, 1},
+    {0, 0, 0, 0},
+  };
+  for (const auto tuplet: tuplets) {
+    const auto [a, b, c, expected] = tuplet;
+    CHECK(triple_max(a, b, c) == expected);
+  }
 }
 
 TEST_CASE("testing the branch coverage reporting for paths nested across lines via Catch2") {
-    std::vector<std::tuple<int, int>> tuplets = {
-        // a, expected
-        {0, 42},
-        {1, 42},
-        {21, 42},
-        {22, 44},
-    };
-    for (const auto tuplet: tuplets) {
-        const auto [a, expected] = tuplet;
-        CHECK(spread_logic(a) == expected);
-    }
+  std::vector<std::tuple<int, int>> tuplets = {
+    // a, expected
+    {0, 42},
+    {1, 42},
+    {21, 42},
+    {22, 44},
+  };
+  for (const auto tuplet: tuplets) {
+    const auto [a, expected] = tuplet;
+    CHECK(spread_logic(a) == expected);
+  }
 }
 
 TEST_CASE("testing the branch coverage reporting for switch paths via Catch2") {
-    std::vector<std::tuple<int, int>> tuplets = {
-        // a, expected
-        {0, 42},
-        {1, 42},
-        {21, 42},
-        {22, 44},
-    };
-    for (const auto tuplet: tuplets) {
-        const auto [a, expected] = tuplet;
-        CHECK(switch_logic(a) == expected);
-    }
+  std::vector<std::tuple<int, int>> tuplets = {
+    // a, expected
+    {0, 42},
+    {1, 42},
+    {21, 42},
+    {22, 44},
+  };
+  for (const auto tuplet: tuplets) {
+    const auto [a, expected] = tuplet;
+    CHECK(switch_logic(a) == expected);
+  }
 }
 
